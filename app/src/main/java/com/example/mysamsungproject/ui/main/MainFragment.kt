b88531dd656d095826ff40a261458dabc2a0b9df
Владимир Романенко <romanenko2004@bk.ru>
2024-03-28 13:04:00 +0300

    Внесены корректировочные изменения в разметку объекта recyclerview

--- app/src/main/java/com/example/mysamsungproject/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mysamsungproject/ui/main/MainFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mysamsungproject.MyListAdapter
 import com.example.mysamsungproject.R
